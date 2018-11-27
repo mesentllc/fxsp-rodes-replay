7f00f820c344ec591b9a5efb23fbc97d4879ac0a
Michael Suess <796812@fedex.com>
2018-11-27 14:09:27 -0600

    1.1.1 - Moved to Spring 5.1.1 - Removed useless (and failing) test.

--- src/test/java/TestTimeCollector.java
+++
@@ -1,6 +0,0 @@
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class TestTimeCollector {
-	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-");
-}
